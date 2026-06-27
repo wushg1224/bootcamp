e9c7920e9619249950656313451a4ec8a095d133
Ceci <165979159+wushg1224@users.noreply.github.com>
2026-06-27 21:07:34 +1000

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          hello world
         </p>
         <a
           className="App-link"
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
     </div>
   );
