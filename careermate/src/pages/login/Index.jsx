271c415e6e6ce2d0056476af5fd9cddd1158af6f
Ceci <165979159+wushg1224@users.noreply.github.com>
2026-07-06 21:38:14 +1000

    login page

--- careermate/src/pages/login/Index.jsx
+++ careermate/src/pages/login/Index.jsx
@@ -1,13 +1,51 @@
 import React from "react";
 import "./Index.css";
+import { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  // console.log("current email state:", email);
+
+  // a function that checks the email format and updates emailError
+ const handleEmailChange = (e) => {
+  const value = e.target.value;
+  setEmail(value);
+
+if (!value.trim()) {
+  setEmailError("Email is required");
+} else if (!value.includes("@")) {
+  setEmailError("Invalid email format");
+} else if (value.length > 50) {
+  setEmailError("Email must be less than 50 characters");
+} else {
+  setEmailError("");
+}
+};
+
   return (
     <div className="login-container">
       <h1>Login</h1>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+      <div className="field">
+      <input type="email" 
+      placeholder="Email"  
+      value={email}
+      autoComplete="off"
+      onChange={handleEmailChange}
+      />
+      {emailError && <p className="error-message">{emailError}</p>}
+      {/* <p>Email: {email}</p> */}
+      </div>
+      
+      <input type="password" placeholder="Password" 
+      value={password}
+      onChange={(e)=> {
+    setPassword(e.target.value);
+    // console.log("password input:", e.target.value);
+  }}/>
       <button>Login</button>
+    
     </div>
   );
 }
