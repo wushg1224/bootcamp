1def3992499219d987e2bd5bf3ae5c7bf099aa29
Ceci <165979159+wushg1224@users.noreply.github.com>
2026-07-07 13:50:10 +1000

    mocklogin function and status setup

--- careermate/src/pages/login/Index.jsx
+++ careermate/src/pages/login/Index.jsx
@@ -2,11 +2,24 @@ import React from "react";
 import "./Index.css";
 import { useState } from "react";
 
+function mockLogin(email, password) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (email === "ceci@gmail.com" && password === "123456") {
+        resolve();
+      } else {
+        reject(new Error("Incorrect email or password"));
+      }
+    }, 1000);
+  });
+}
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+  const [status, setStatus] = useState("idle"); // idle | loading | success | error
+const [error, setError] = useState("");
 
   // console.log("current email state:", email);
 
@@ -42,8 +55,39 @@ if (!value.trim()) {
   setPasswordError("");
 }};
 
+//function for validate 2 fileds before form submit
+function validate(email, password) {
+  if (!email.trim() || !password.trim()) return "All fields are required";
+  if (!email.includes("@")) return "Invalid email format";
+  if (password.length < 6) return "Password must be at least 6 characters";
+  return null;
+}
+
+
+  async function handleSubmit(e) {
+  e.preventDefault();
+  setError("");
+
+  const errMsg = validate(email, password);
+  if (errMsg) {
+    setStatus("error");
+    setError(errMsg);
+    return;
+  }
+
+  try {
+    setStatus("loading");
+    await mockLogin(email, password);   // pauses here ~1 second
+    setStatus("success");
+  } catch (err) {
+    setStatus("error");
+    setError(err.message);              // "Incorrect email or password"
+  }
+}
+
+
   return (
-    <div className="login-container">
+    <form className="login-container" onSubmit={handleSubmit}>
       <h1>Login</h1>
       <div className="field">
       <input type="email" 
@@ -63,10 +107,16 @@ if (!value.trim()) {
       onChange={passwordChange}/>
       {passwordError && <p className="error-message">{passwordError}</p>}
       </div>
+{status === "error" && <p className="error-message">{error}</p>}
 
-      <button>Login</button>
+{/* //button UI react to status */}
+      <button disabled={status === "loading"}>
+  {status === "loading" ? "Logging in..." : "Login"}
+</button>
+
+{status === "success" && <p className="success-message">Login success ✅</p>}
     
-    </div>
+    </form>
   );
 }
 
