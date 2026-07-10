5719264d8b57ec1ec753c05659365c6ce7e767a2
Ceci <165979159+wushg1224@users.noreply.github.com>
2026-07-10 14:14:17 +1000

    A counter component on home page for practice

---
+++ careermate/src/components/Counter/Counter.jsx
@@ -0,0 +1,22 @@
+//import css file
+import "./Counter.css";
+
+//import useState hook
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount((prevCount) => prevCount + 1);
+    console.log("Count:", count);
+  }
+
+  return (
+    <div className="counter">
+      <h1>Counter</h1>
+      <h2 className="count">{count}</h2>
+      <button onClick={handleClick}>Add</button>
+    </div>
+  );
+}
--- careermate/src/pages/home/home.jsx
+++ careermate/src/pages/home/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./home.css";
+import Counter from "../../components/Counter/Counter";
 
 function Home() {
   return (
     <div className="home-container">
       <h1>Welcome to CareerMate!</h1>
       <p>You have successfully logged in.</p>
+      <Counter />
     </div>
   );
 }
