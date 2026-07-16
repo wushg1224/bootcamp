02c9a19b7f45b6e7c2121ed135a5ffd0d7e37716
Ceci <165979159+wushg1224@users.noreply.github.com>
2026-07-16 13:01:28 +1000

    feat: add fail-fast validation guard to POST /movies
    feat: add fail-fast validation guard to POST /movies
    
    format

---
+++ node-backend/lecture2/movie-api/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+
+const app = express();
+
+// express can parse json File
+app.use(express.json());
+
+const movieRouter = express.Router();
+//router mounting
+app.use("/v1/movies", movieRouter);
+
+//temparory database only exits when server start
+const movies = [
+  {
+    id: 0,
+    title: "Inception",
+    description: "A skilled thief steals secrets from dreams.",
+    types: ["Sci-Fi"],
+    averageRating: 4.5,
+    reviews: [{}],
+  },
+];
+let nextId = 1;
+
+//for id generate can use library like uuid and nanoid
+
+// test on interface make sure it is working
+//route inside a router is relative to where the router is mounted
+movieRouter.get("/", (req, res) => {
+  res.json([movies]);
+});
+
+//post --create
+movieRouter.post("/", (req, res) => {
+  const { title, description, types } = req.body;
+  //data validation
+  //fail fast
+  if (!title || !description || !Array.isArray(types) || types.length === 0) {
+    res.status(400).json({
+      message: "all fileds are requeired",
+    });
+    return;
+  }
+
+  const movie = {
+    id: nextId++,
+    title,
+    description,
+    types,
+    averageRating: 0,
+    reviews: [],
+  };
+  movies.push(movie);
+  res.status(201).json(movie);
+});
+
+app.listen(3000, () => {
+  console.log("listening on 3000");
+});
