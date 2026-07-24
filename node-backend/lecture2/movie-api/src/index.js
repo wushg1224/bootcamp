480002d165f431c9fe9e25d49a7ee6c704ec02c1
Ceci <165979159+wushg1224@users.noreply.github.com>
2026-07-24 22:13:26 +1000

    feat: read port from .env, add morgan logging and helmet security headers

--- node-backend/lecture2/movie-api/src/index.js
+++ node-backend/lecture2/movie-api/src/index.js
@@ -1,8 +1,21 @@
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV;
+
+console.log(process.env.PORT); // "4000"
+
 const express = require("express");
 const cors = require("./middleware/cors.middleware");
 const movieRouter = require("./routes/movie.route");
 
 const app = express();
+
+app.use(morgan(NODE_ENV === "production" ? "combined" : "dev"));
+app.use(helmet());
 
 // express can parse json File
 app.use(express.json());
@@ -12,6 +25,6 @@ app.use(cors);
 app.use("/v1/movies", movieRouter);
 
 //port watching
-app.listen(3000, () => {
-  console.log("listening on 3000");
+app.listen(port, () => {
+  console.log(`server listening on ${port}`);
 });
